db1365e79962096d3122b4fea6ae0bbe8b5eb318
jasercion <jasercion.gwu@gmail.com>
2017-09-05 17:17:17 -0400

    add needed header file
    this will live here until I find a better place for it

---
+++ optimizers/RosenBounded.h
@@ -0,0 +1,56 @@
+/** 
+ * @file RosenBounded.h
+ * @brief Declaration for a 2D Rosenbrock objective function
+ * @author J. Chiang
+ *
+ * $Header: /heacvs/glast/ScienceTools/glast/optimizers/src/RosenBounded.h,v 1.1.2.7 2017/02/01 04:26:34 jasercio Exp $
+ */
+
+#include "optimizers/Statistic.h"
+
+namespace optimizers {
+    
+class RosenBounded : public Statistic {
+
+public:
+
+   RosenBounded(double prefactor=100);
+      
+   virtual double value() const {
+      Arg dummy;
+      return value(dummy);
+   }
+
+   virtual void getFreeDerivs(std::vector<double>  & derivs) const {
+      Arg dummy;
+      Function::getFreeDerivs(dummy, derivs);
+   }
+
+   virtual Function * clone() const {
+      return new RosenBounded(*this);
+   }
+
+
+   void set_xbounds(double xmin, double xmax);
+   void set_ybounds(double ymin, double ymax);
+
+protected:
+
+   virtual double value(const Arg &) const;
+
+   virtual double derivByParamImp(const Arg &,
+                                  const std::string & paramName) const;
+
+private:
+
+   double m_prefactor;
+
+   double m_xmin, m_xmax;
+   double m_ymin, m_ymax;
+
+   void check_bounds(double x, double y) const;
+
+};
+
+} // namespace optimizers
+
