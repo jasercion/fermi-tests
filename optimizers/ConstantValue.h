4d3074330d8b2aadead180db3b066d84fd73d78b
jasercion <jasercion.gwu@gmail.com>
2017-09-05 17:06:53 -0400

    add needed header file
    this will live here until I find a better place for it

---
+++ optimizers/ConstantValue.h
@@ -0,0 +1,53 @@
+/** 
+ * @file ConstantValue.h
+ * @brief Declaration for the ConstantValue Function class
+ * @author J. Chiang
+ *
+ * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/optimizers/src/ConstantValue.h,v 1.8 2015/03/21 05:36:46 jchiang Exp $
+ */
+
+#ifndef optimizers_ConstantValue_h
+#define optimizers_ConstantValue_h
+
+#include "optimizers/Function.h"
+
+namespace optimizers {
+
+/** 
+ * @class ConstantValue
+ *
+ * @brief This returns a constant value, the sole Parameter of this class,
+ * regardless of the value or type of Arg.
+ *
+ */
+    
+class ConstantValue : public Function {
+
+public:
+
+   ConstantValue(double value=1) 
+      : Function("ConstantValue", 1, "Value", "dArg", Addend) {
+      addParam("Value", value, true);
+   }
+
+   virtual ~ConstantValue() {}
+
+   virtual Function * clone() const {
+      return new ConstantValue(*this);
+   }
+
+protected:
+
+   double value(const Arg &) const {
+      return m_parameter[0].getTrueValue();
+   }
+
+   double derivByParamImp(const Arg &, const std::string &) const {
+      return m_parameter[0].getScale();
+   }
+
+};
+
+} // namespace optimizers
+
+#endif // optimizers_ConstantValue_h
